282a3297d398f40f67480076003f74bc17054877
KCSPreethi <preethik@sahaj.ai>
2023-02-20 12:27:40 +0530

    Change the method in vehicle to include vehicle number

--- src/main/kotlin/models/Vehicle.kt
+++ src/main/kotlin/models/Vehicle.kt
@@ -1,34 +1,44 @@
 package models
 
+import services.TicketDispenser
 import java.time.Duration
 import java.time.LocalDateTime
 
-class Vehicle(private val slot: SlotsSchedules) {
+class Vehicle(private val vehicleNumber:Int) {
     //Ticket number
     //Receipt number
     //vehicle number
-    fun parkVehicle(): Ticket {
-        val slotNumber = slot.assignSlotToVehicle()
-        val ticket = Ticket(slotNumber)
-        //Call ticket dispenser
-        //return ticket
-        return ticket.generateTicketForVehicle()
+//    fun parkVehicle(): Ticket {
+//        val slotNumber = slot.assignSlotToVehicle()
+//        val ticket = Ticket(slotNumber)
+//        //Call ticket dispenser
+//        //return ticket
+//        return ticket.generateTicketForVehicle()
+//
+//    }
 
-    }
-
-    fun unParkVehicle(ticket: Ticket): Receipt {
-        val exitTime = LocalDateTime.now()
-        slot.unBookSlot(ticket)
-        val receipt = Receipt(ticket)
-        val parkingFee = getDuration(ticket.getEntryTime(), exitTime) * 100
-        return receipt.generateReceiptDuringUnParking(exitTime, parkingFee)
-
-    }
+//    fun unParkVehicle(ticket: Ticket): Receipt {
+//        val exitTime = LocalDateTime.now()
+//        slot.unBookSlot(ticket)
+//        val receipt = Receipt(ticket)
+//        val parkingFee = getDuration(ticket.getEntryTime(), exitTime) * 100
+//        return receipt.generateReceiptDuringUnParking(exitTime, parkingFee)
+//
+//    }
 
     private fun getDuration(entryTime: LocalDateTime, exitTime: LocalDateTime): Long {
         val days = Duration.between(entryTime, exitTime).toDays()
         return (days * 24) + Duration.between(entryTime, exitTime).toHours()
 
+    }
+
+    fun park(slotNumber: Int): Ticket {
+        val ticketDispenser = TicketDispenser()
+        return ticketDispenser.getTicket(  slotNumber,this)
+
+    }
+    fun getVehicleNumber(): Int {
+        return vehicleNumber
     }
 
 }
